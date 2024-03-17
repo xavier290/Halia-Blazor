6a4b4fa18a5849a294e9654dd7cd2f7c6b8b530b
xavier290 <xa25alex@gmail.com>
2024-03-17 13:04:07 -0600

    Code refactored for login and main component

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using NovaLaundryAppWebAdminBlazor.Server.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NovaLaundryAppWebAdminBlazor.ModelsHalia;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+// using NovaLaundryAppWebAdminBlazor.Data;
+// using NovaLaundryAppWebAdminBlazor.Server.Interfaces;
 
 string credentialPath = @"C:\Users\Usuario\source\repos\Credentials\halia-82cdf-firebase-adminsdk-ygtoo-a51e4f10e5.json";
 Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
@@ -16,6 +24,20 @@ builder.Services.AddBlazoredToast(); // Registro de Blazored.Toast
 builder.Services.AddSweetAlert2();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+
+// var connectionString = builder.Configuration.GetConnectionString("Default");
+// builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+
+// builder.Services.AddIdentity<Usuario, IdentityRole>(options => {
+//     options.Password.RequiredLength = 2;
+//     options.Password.RequireNonAlphanumeric = false;
+//     options.Password.RequireDigit = false;
+//     options.Password.RequireLowercase = false;
+//     options.Password.RequireUppercase = false;
+// })
+// .AddEntityFrameworkStores<DataContext>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 var app = builder.Build();
 
@@ -32,6 +54,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+// app.UseAuthentication();
+// app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
---
+++ Server/Services/AuthenticationService.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NovaLaundryAppWebAdminBlazor.Server.Services;
+using NovaLaundryAppWebAdminBlazor.Server.ViewModels;
+using NovaLaundryAppWebAdminBlazor.ModelsHalia;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Blazored.LocalStorage;
+
+
+public class AuthenticationService : IAuthenticationService
+{
+
+    private readonly ILocalStorageService _localStorage;
+
+    public AuthenticationService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+    public async Task<bool> LoginAsync(string username, string password)
+    {
+        try
+        {
+            using (SystemAdminContext db = new SystemAdminContext()) 
+            {
+                // Check if username and password are provided
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    return false;
+                }
+
+                // Retrieve user from the database based on username and password
+                Usuario user = db.Usuarios.FirstOrDefault(u => u.Nombre == username && u.Pass == password);
+
+                // If user is null, authentication failed
+                if (user == null || user.Estado == "Bloqueado")
+                {
+                    return false;
+                }
+
+                await _localStorage.SetItemAsync("IsAuthenticated", true);
+                await _localStorage.SetItemAsync("user", user.PrimerNombre.ToString() + " " + user.PrimerApellido.ToString());
+                await _localStorage.SetItemAsync("userId",user.IdUsuarios.ToString());
+                await _localStorage.SetItemAsync("sucursalId",user.SucursalId.ToString());
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            // Log any exceptions
+            Console.WriteLine($"An error occurred during authentication: {ex.Message}");
+            return false;
+        }
+    }
+
+    public async Task<bool> LogoutAsync()
+    {
+        try
+        {
+            // Clear authentication status from local storage
+            await _localStorage.RemoveItemAsync("IsAuthenticated");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            // Log and handle exceptions
+            Console.WriteLine($"An error occurred during logout: {ex.Message}");
+            return false;
+        }
+    }
+    public async Task<bool> IsAuthenticatedAsync()
+    {
+        try
+        {
+            // Retrieve authentication status from local storage
+            return await _localStorage.GetItemAsync<bool>("IsAuthenticated");
+        }
+        catch (Exception ex)
+        {
+            // Log and handle exceptions
+            Console.WriteLine($"An error occurred while checking authentication status: {ex.Message}");
+            return false;
+        }
+    }
+}
