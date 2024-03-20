28bc759a09e588c691552c4830e4b6e0ee56e148
xavier290 <xa25alex@gmail.com>
2024-03-20 14:05:27 -0600

    create and update functionality ready for providers product lines, and products

--- Server/Interfaces/IcatalogService.cs
+++ Server/Interfaces/IcatalogService.cs
@@ -9,7 +9,21 @@ public interface IcatalogService
 {
     // funcitions for CRUD products
     Task AddProductAsync(string name, decimal price, string productService, string code, string description);
+    Task AddProductLineAsync(string name);
+    Task AddProviderAsync(string name, string telefono, string direccion, string pais);
+    
+    
     Task UpdateProductAsync(int entryId, string name, decimal price, string productService, string code, string description);
+    Task UpdateProductLineAsync(int entryId, string name);
+    Task UpdateProviderAsync(int entryId, string name, string telefono, string direccion, string pais);
+    
+    
     Task<List<List<object>>> GetProductAsync(string filter);
+    Task<List<List<object>>> GetProductLineAsync(string filter);
+    Task<List<List<object>>> GetProviderAsync(string filter);
+    
+    
     Task<Producto> GetSingleProductAsync(int entryId);
+    Task<Linea> GetSingleProductLineAsync(int entryId);
+    Task<Proveedor> GetSingleProviderAsync(int entryId);
 }
--- Server/Services/CatalogService.cs
+++ Server/Services/CatalogService.cs
@@ -82,4 +82,131 @@ public class CatalogService : IcatalogService
             db.SaveChanges();
         }
     }
+
+    public async Task<List<List<object>>> GetProductLineAsync(string filter) 
+    {
+        List<List<object>> rows = new List<List<object>>();
+
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            List<Linea> lineaList = db.Lineas.Where(e => e.Nombre.Contains(filter)).ToList();
+
+            foreach (var item in lineaList)
+            {
+                List<object> fila = new List<object>();
+                fila.Add(item.LineaId);
+                fila.Add(item.Nombre);
+
+                rows.Add(fila);
+            }
+        }
+
+        return rows;
+    }
+
+    public async Task<Linea> GetSingleProductLineAsync(int entryId)
+    {
+        using (HaliabdContext db = new HaliabdContext())
+        {
+            Linea linea = await db.Lineas.FirstOrDefaultAsync(e => e.LineaId == entryId);
+            return linea;
+        }
+    }
+
+    public async Task AddProductLineAsync(string name) 
+    {
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            Linea linea = new Linea()
+            {
+                Nombre = name.Trim(), 
+            };   
+
+            db.Add(linea);
+            db.SaveChanges();
+        }
+    }
+
+    public async Task UpdateProductLineAsync(int entryId, string name) 
+    {
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            Linea linea = db.Lineas.Find(entryId);
+
+            if (linea != null)
+            {
+                linea.Nombre = name.Trim();    
+            }
+
+            db.SaveChanges();
+        }
+    }
+
+    public async Task<List<List<object>>> GetProviderAsync(string filter) 
+    {
+        List<List<object>> rows = new List<List<object>>();
+
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            List<Proveedor> proveedorList = db.Proveedors.Where(e => e.NombreProveedor.Contains(filter)).ToList();
+
+            foreach (var item in proveedorList)
+            {
+                List<object> fila = new List<object>();
+                fila.Add(item.ProveedorId);
+                fila.Add(item.NombreProveedor);
+                fila.Add(item.NumeroTelefono);
+                fila.Add(item.Direccion);
+                fila.Add(item.Pais);
+
+                rows.Add(fila);
+            }
+        }
+
+        return rows;
+    }
+
+    public async Task<Proveedor> GetSingleProviderAsync(int entryId)
+    {
+        using (HaliabdContext db = new HaliabdContext())
+        {
+            Proveedor proveedor = await db.Proveedors.FirstOrDefaultAsync(e => e.ProveedorId == entryId);
+            return proveedor;
+        }
+    }
+
+    public async Task AddProviderAsync(string name, string telefono, string direccion, string pais) 
+    {
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            Proveedor proveedor = new Proveedor()
+            {
+                NombreProveedor = name.Trim(), 
+                NumeroTelefono = telefono.Trim(),
+                Direccion = direccion.Trim(),
+                Pais = pais.Trim()
+            };   
+
+            db.Add(proveedor);
+            db.SaveChanges();
+        }
+    }
+
+    public async Task UpdateProviderAsync(int entryId, string name, string telefono, string direccion, string pais) 
+    {
+        using(HaliabdContext db = new HaliabdContext())
+        {
+            Proveedor proveedor = db.Proveedors.Find(entryId);
+
+            if (proveedor != null)
+            {
+                proveedor.NombreProveedor = name.Trim();
+                proveedor.NumeroTelefono = telefono.Trim();
+                proveedor.Direccion = direccion.Trim();
+                proveedor.Pais = pais.Trim();
+            }
+
+            db.SaveChanges();
+        }
+    }
 }
